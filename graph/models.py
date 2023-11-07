81d83e8bf99a25a02cd4f60630481ed252514511
Ogwolfe <125140029+Ogwolfe@users.noreply.github.com>
2023-11-07 23:38:18 +0000

    Added GraphModel

--- graph/models.py
+++ graph/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class GraphModel(models.Model):
+    graph_name  = models.CharField(max_length=20, help_text='Enter graph name')
+
+    def __str__(self):
+        return self.graph_name
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of the model."""
+        return reverse('model-detail-view', args=[str(self.id)])
+    
+    
