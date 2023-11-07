75a3a0c62843ad26eed8a92e78ce38a8092f1f0c
Ogwolfe <125140029+Ogwolfe@users.noreply.github.com>
2023-11-07 20:35:06 +0000

    test

--- GraphSonification/urls.py
+++ GraphSonification/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graph/', include('graph.urls')),
 ]
+
+#Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='graph/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
