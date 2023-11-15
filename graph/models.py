25f4a639622779a1e30f4586ff4c3c242f39d2d1
Ogwolfe <125140029+Ogwolfe@users.noreply.github.com>
2023-11-15 20:08:03 +0000

    Added form to get input for graph name and added graph_creation page

--- graph/models.py
+++ graph/models.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 # Create your models here.
 class GraphModel(models.Model):
     graph_name  = models.CharField(max_length=20, help_text='Enter graph name')
+    Xlabel = models.CharField(max_length=20, help_text='Enter x-axis label')
+    Ylabel = models.CharField(max_length=20, help_text='Enter y-axis label')
+    X_value = models.AutoField()
+    Y_value = models.AutoField()
 
     def __str__(self):
         return self.graph_name
