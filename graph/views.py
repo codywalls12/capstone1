f7e4226d6e8f7d8eb09bb94a2650149fe1cf167a
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2023-11-15 19:24:16 -0600

    Update views.py
    Updated to have graph be created with user info

--- graph/views.py
+++ graph/views.py
@@ -4,27 +4,26 @@ from .models import GraphModel, graphpoint
 from bokeh.embed import components
 from bokeh.plotting import figure
 
-
-
-
 def index(request):
     context = {}
     return render(request, "graph/index.html", context)
 
-
 def graph_creation(request):
     graph_name = request.POST.get('graph_name')
     test_graph = GraphModel.objects.all()
-
-    plot = figure(width=400, height=400)
-    plot.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
-    script, div = components(plot)
-
-
+   
+    x_val = request.POST.get('x_values')
+    y_val= request.POST.get('y_values')
+    
+    y_Values= [int(d) for d in y_val.split(',')]
+    x_Values= x_val.split(',')
+    p = figure(x_range=x_Values, height=350, title="Fruit Counts", toolbar_location=None, tools="")
+    p.vbar(x=x_Values, top=y_Values, width=0.9)
+    script, div = components(p)
 
     context = {'test_graph': test_graph,
                'graph_name': graph_name,
                'script': script,
                'div': div,}
-    
+
     return render(request, "graph/graph_creation.html", context)
