085d513941ccc07e91dac2d8a3ab575253e2d01e
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2023-11-30 18:02:27 -0600

    Update views.py
    Changed logic to allow for x and y pair of information.

--- graph/views.py
+++ graph/views.py
@@ -9,21 +9,21 @@ def index(request):
     return render(request, "graph/index.html", context)
 
 def graph_creation(request):
-    graph_name = request.POST.get('graph_name')
-    test_graph = GraphModel.objects.all()
-   
-    x_val = request.POST.get('x_values')
-    y_val= request.POST.get('y_values')
     
-    y_Values= [int(d) for d in y_val.split(',')]
-    x_Values= x_val.split(',')
-    p = figure(x_range=x_Values, height=350, title=graph_name, toolbar_location=None, tools="")
-    p.vbar(x=x_Values, top=y_Values, width=0.9)
-    script, div = components(p)
+        graph_name = request.POST.get('graph_name')
+        x_values = request.POST.getlist('x_values[]')
+        y_values = request.POST.getlist('y_values[]')
 
-    context = {'test_graph': test_graph,
-               'graph_name': graph_name,
-               'script': script,
-               'div': div,}
+        p = figure(x_range=x_values, height=350, title=graph_name, toolbar_location=None, tools="")
+        p.vbar(x=x_values, top=y_values, width=0.9)
+        script, div = components(p)
 
-    return render(request, "graph/graph_creation.html", context)
+        context = {
+            'graph_name': graph_name,
+            'script': script,
+            'div': div,
+        }
+
+        return render(request, "graph/graph_creation.html", context)
+
+    
