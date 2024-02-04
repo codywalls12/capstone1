8e2220f0ff46f878407d11f051e199be206a89dd
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2024-02-04 15:37:58 -0600

    Upload current work
    Update main page, finalize manual data input for current version, update buttons, update logo and logo redirects to home

--- graph/views.py
+++ graph/views.py
@@ -9,21 +9,24 @@ def index(request):
     return render(request, "graph/index.html", context)
 
 def graph_creation(request):
-    
-        graph_name = request.POST.get('graph_name')
-        x_values = request.POST.getlist('x_values[]')
-        y_values = request.POST.getlist('y_values[]')
+    graph_name = request.POST.get('graph_name')
+    x_values = request.POST.getlist('x_values[]')
+    y_values = request.POST.getlist('y_values[]')
 
-        p = figure(x_range=x_values, height=350, title=graph_name, tools="pan,box_zoom,wheel_zoom,reset,save", toolbar_location="right")
-        p.vbar(x=x_values, top=y_values, width=0.9)
-        script, div = components(p)
+    if not x_values or not y_values:
+        return HttpResponse("Error: x values or y values are missing")
 
-        context = {
-            'graph_name': graph_name,
-            'script': script,
-            'div': div,
-        }
+    if len(x_values) != len(y_values):
+        return HttpResponse("Error: Number of x values and y values do not match")
 
-        return render(request, "graph/graph_creation.html", context)
+    p = figure(x_range=x_values, height=350, title=graph_name, x_axis_label='X Values', y_axis_label='Y Values', tools="pan,box_zoom,wheel_zoom,reset,save", toolbar_location="right")
+    p.vbar(x=x_values, top=y_values, width=0.9)
+    script, div = components(p)
 
-    
+    context = {
+        'graph_name': graph_name,
+        'script': script,
+        'div': div,
+    }
+
+    return render(request, "graph/graph_creation.html", context)
