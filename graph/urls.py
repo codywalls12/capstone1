f33295af92fb32ff013e3a1d175ffb76daa42fc3
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2024-03-16 16:05:48 -0500

    Settings image update
    Added the new versions for the images for each of the colorblind options

--- graph/urls.py
+++ graph/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "graph"
@@ -6,6 +7,4 @@ app_name = "graph"
 urlpatterns = [
     path("", views.index, name="index"),
     path("test", views.graph_creation, name="graph_creation"),
-    path('about_us/', views.about_us, name='about_us'),
-
 ]
