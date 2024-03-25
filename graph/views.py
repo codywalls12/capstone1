b030ae64dcfdf3886b4ece7f21cc22646fa8aa0f
Ogwolfe <125140029+Ogwolfe@users.noreply.github.com>
2024-03-25 15:43:02 +0000

    Updated views.py so original excel_upload function uses context and generates graph.

--- graph/views.py
+++ graph/views.py
@@ -131,7 +131,6 @@ def graph_creation(request):
 def excel_upload(request):
     print("Entered excel_upload")
     if request.method == 'POST':
-        print("Entered request.method==post")
         form = ExcelDataForm(request.POST, request.FILES)
         if form.is_valid():
             print("Form is valid")
@@ -140,13 +139,59 @@ def excel_upload(request):
             except StopIteration:
                  print("No file was uploaded!")
             form.save()
-            print(form.instance.filename())
-            df = pd.read_excel("graph/static/graph/" + form.instance.filename())
-            first_column = df.iloc[:, 1]
-            print(first_column)
+            #Check if excel file
+            if form.instance.filename().endswith(".xlsx"):
+                df = pd.read_excel("graph/static/graph/" + form.instance.filename())
+
+            #Check if csv file
+            elif form.instance.filename().endswith(".csv"):
+                df = pd.read_csv("graph/static/graph/" + form.instance.filename())
+
+            graph_x_axis = df.iloc[:, form.instance.x_values]
+            graph_y_axis = df.iloc[:, form.instance.y_values]
+
+            print(graph_x_axis, graph_y_axis)
+
+
+            p = figure(x_range=graph_x_axis, height=350, title="Test", x_axis_label="X Values", y_axis_label='Y Values', tools="pan,box_zoom,wheel_zoom,reset,save", toolbar_location="right")
+            p.vbar(x=graph_x_axis, top=graph_y_axis, width=0.9)
+            script, div = components(p)
+
+
+            # Create file path to save .wav file to static folder
+            save_path = "graph/static/graph"
+            file_name = "graph_audio.wav"
+            graph_audio = os.path.join(save_path, file_name)
+
+            # Create an instance of the ToneGenerator
+            tone = ToneGenerator()
+
+            # Create a list to hold the values for frequencies
+            frequencies = []
+
+            # This for loop goes throught the range of the y values entered by the user and adds them to the frequencies list
+            for i in range(len(graph_y_axis)):
+                frequencies.append(graph_y_axis[i])
+            mellody = []
+            for i in range(len(frequencies)):
+                mellody += list(tone.render(0.5, int(frequencies[i]), "sin"))
+            
+            # Once the mellody list has been created we write it to the proper file as a .wav file
+            ToneGenerator.write_to_file(np.array(mellody), graph_audio)
+
+
+
+
+            context = {
+                'script': script,
+                'div': div,
+                'form': form
+            }
+
+
     else:
         print("request not POST")
         form = UploadForm()
-    return render(request, 'graph/graph_creation.html', {'form': form})
+    return render(request, 'graph/graph_creation.html', context)
 
 
